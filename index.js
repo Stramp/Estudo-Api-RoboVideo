151091419bb016d9ef12f27eb01fcba8577d2fea
Stramp <rafaelcostart2@gmail.com>
2020-05-12 02:30:08 -0300

    instalado aprser trends mod

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const readline = require('readline-sync')
+const Parser = require('rss-parser');
+
+const TREND_URL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR' 
+
 function start(){
  const content = {}
  content.searchTerm = askAndReturnSearchTerm()
  content.prefix = askAndReturnPrefix()
 
- function askAndReturnSearchTerm(){
-     return readline.question('escolha:')
- }
+ async function askAndReturnSearchTerm () {
+    const response = readline.question('Type a Wikipedia search term or G to fetch google trends: ')
+
+    return (response.toUpperCase() === 'G') ?  await askAndReturnTrend() : response
+       
+  }
+
+  async function askAndReturnTrend() {
+    console.log('Please Wait...')
+    const trends = await getGoogleTrends()
+    const choice = readline.keyInSelect(trends, 'Choose your trend:')
+    
+    return trends[choice] 
+
+  }
+
+  async function getGoogleTrends () {
+    const parser = new Parser()
+    const trends = await parser.parseURL(TREND_URL)
+    return trends.items.map(({title}) => title)
+  }
+
  function askAndReturnPrefix(){
     const prefix = ['Quem é','A historia','Fatos Sobre','Oque Fez']
-    const selectedPrefix = readline.keyInSelect(prefix)
+    const selectedPrefix = readline.keyInSelect(prefix,`Choose an option for ${content.searchTerm}:`)
     return prefix[selectedPrefix];
  }
  console.log(content);
