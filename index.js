ca314b6052fddbc4c417638a54c04265f623ec2f
Stramp <rafaelcostart2@gmail.com>
2020-05-12 01:58:40 -0300

    feito a parte de escolher assunto manualmente

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const readline = require('readline-sync')
 function start(){
  const content = {}
  content.searchTerm = askAndReturnSearchTerm()
+ content.prefix = askAndReturnPrefix()
 
  function askAndReturnSearchTerm(){
-     return "teste"
+     return readline.question('escolha:')
+ }
+ function askAndReturnPrefix(){
+    const prefix = ['Quem é','A historia','Fatos Sobre','Oque Fez']
+    const selectedPrefix = readline.keyInSelect(prefix)
+    return prefix[selectedPrefix];
  }
  console.log(content);
 }
 
-start();
+start(); 
